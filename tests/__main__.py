4534dfe2bf752120ac4e8d01cabc7bf1685b91f9
Subhaneil Lahiri <subhylahiri@gmail.com>
2020-05-08 21:51:22 -0700

    renamed files to order them

--- tests/__main__.py
+++ tests/__main__.py
@@ -5,6 +5,6 @@ from numpy_linalg.testing import main
 
 this_dir = os.path.dirname(__file__)
 executable = os.path.basename(sys.executable)
-argv = [executable + '-m' + 'tests', 'discover', '-s', this_dir] + sys.argv[1:]
+argv = [executable + ' -m tests', 'discover', '-s', this_dir] + sys.argv[1:]
 
 main(module=None, argv=argv)
